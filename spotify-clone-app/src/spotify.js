cc3a1a90bc3dd62ccc59ee036f7a2e3519a3035c
MeetHrishii <neverendiingdreams@gmail.com>
2022-10-11 18:57:06 +0530

    Login functionality & Authorization added

--- spotify-clone-app/src/spotify.js
+++ spotify-clone-app/src/spotify.js
@@ -1,6 +1,26 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-
+const clientId = "cff76213089a4e228206c8bf120d67cb";
 const redirectUri = "http://localhost:3000/";
-// This is the URI I created so when authentication is done by 
-// spotifys side it's gonna redirect to this URI
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
 
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
