1e307f9f8bc7f8b991dad99ed4dca8d9eb598b35
MeetHrishii <neverendiingdreams@gmail.com>
2022-10-11 17:31:22 +0530

    Login functionality created

---
+++ spotify-clone-app/src/spotify.js
@@ -0,0 +1,6 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+// This is the URI I created so when authentication is done by 
+// spotifys side it's gonna redirect to this URI
+
